683217ad2f671d19c25448eeb1f1fe658bbf6eef
braboy2000 <brandonraboy@gmail.com>
2025-04-28 16:33:52 -0700

    testing deploy.yml file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #sets up a URL route
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Hello, World version2!"
 
 if __name__ == "__main__":
     app.run(debug=True)
